559a7018ff9825466047880d3c1542b9480b7afb
jyothisrip8 <jyothisrilakshmi.polisetty@slu.edu>
2025-05-05 23:39:50 +0000

    Lab 7 completed

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react'
+import { BASE_URL } from '../config';
 import Card from './Card'
 import Button from './Button'
 import Search from './Search'
 
-const CardList = ({ data }) => {
-  // define the limit state variable and set it to 10
+const CardList = () => {
   const limit = 10;
+  const [offset, setOffset] = useEffect(limit);
+  // define the limit state variable and set it to 10
+  const [products, setProducts] = useState();
 
-  // Define the offset state variable and set it to 0
-  const [offset, setOffset] = useState(0);
-  // Define the products state variable and set it to the default dataset
-  const [products, setProducts] = useState(data);
-
-  useEffect(() => {
-    setProducts(data.slice(offset, offset + limit));
-  }, [offset, limit, data])
 
   const filterTags = (tagQuery) => {
     const filtered = data.filter(product => {
@@ -29,6 +24,17 @@ const CardList = ({ data }) => {
     setProducts(filtered)
   }
 
+  const fetchProducts = () => {
+    fetch(`${BASE_URL}/products?offset=${offset}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [offset]);
 
   return (
     <div className="cf pa2">
